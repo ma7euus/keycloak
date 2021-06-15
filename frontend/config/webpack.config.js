4e079159299f4802acbeb806e0b0ff57b72b54c1
Mateus <mateuspastori@outlook.com>
2021-06-14 22:24:42 -0300

    Personalizando login keycloak react app

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -30,7 +30,7 @@ const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'
 const postcssNormalize = require('postcss-normalize');
 
 const appPackageJson = require(paths.appPackageJson);
-
+const CopyPlugin = require('copy-webpack-plugin');
 // Source maps are resource heavy and can cause out of memory issue for large source files.
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
 
@@ -227,14 +227,14 @@ module.exports = function (webpackEnv) {
                 // There will be one main bundle, and one file per asynchronous chunk.
                 // In development, it does not produce real files.
                 filename: isEnvProduction
-                    ? 'static/js/[name].[contenthash:8].js'
-                    : isEnvDevelopment && 'static/js/bundle.js',
+                    ? theme.themeDir + '/js/[name].[contenthash:8].js'
+                    : isEnvDevelopment && theme.themeDir + '/js/bundle.js',
                 // TODO: remove this when upgrading to webpack 5
                 futureEmitAssets: true,
                 // There are also additional JS chunk files if you use code splitting.
                 chunkFilename: isEnvProduction
-                    ? 'static/js/[name].[contenthash:8].chunk.js'
-                    : isEnvDevelopment && 'static/js/[name].chunk.js',
+                    ? theme.themeDir + '/js/[name].[contenthash:8].chunk.js'
+                    : isEnvDevelopment && theme.themeDir + '/js/[name].chunk.js',
                 // webpack uses `publicPath` to determine where the app is being served from.
                 // It requires a trailing slash, or the file assets will get an incorrect path.
                 // We inferred the "public path" (such as / or /my-project) from homepage.
@@ -445,9 +445,10 @@ module.exports = function (webpackEnv) {
                                                 },
                                             },
                                         ],
-                                        isEnvDevelopment &&
-                                        shouldUseReactRefresh &&
-                                        require.resolve('react-refresh/babel'),
+                                        // não precisa de hot reload
+                                        //isEnvDevelopment &&
+                                        //shouldUseReactRefresh &&
+                                        //require.resolve('react-refresh/babel'),
                                     ].filter(Boolean),
                                     // This is a feature of `babel-loader` for webpack (not Babel itself).
                                     // It enables caching results in ./node_modules/.cache/babel-loader/
@@ -570,9 +571,9 @@ module.exports = function (webpackEnv) {
                                 // its runtime that would otherwise be processed through "file" loader.
                                 // Also exclude `html` and `json` extensions so they get processed
                                 // by webpacks internal loaders.
-                                exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+                                exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/, /\.ftl$/],
                                 options: {
-                                    name: 'static/media/[name].[hash:8].[ext]',
+                                    name: 'media/[name].[hash:8].[ext]',
                                 },
                             },
                             // ** STOP ** Are you adding a new loader?
@@ -583,31 +584,31 @@ module.exports = function (webpackEnv) {
             },
             plugins: [
                 // Generates an `index.html` file with the <script> injected.
-                new HtmlWebpackPlugin(
+                new CopyPlugin({
+                    patterns: [
+                        {
+                            from: path.join(paths.appPublic, theme.themeDir),
+                            to: path.join(paths.appBuild, theme.themeDir),
+                        }
+                    ]
+                }),
+                ...theme.templates.map(template => new HtmlWebpackPlugin(
                     Object.assign(
                         {},
                         {
-                            inject: true,
-                            template: paths.appHtml,
+                            inject: false,
+                            template: template.templateSrc,
+                            chunks: [
+                                template.entry.chunk
+                            ],
+                            filename: path.join(paths.appBuild, template.templateOut),
+
                         },
-                        isEnvProduction
-                            ? {
-                                minify: {
-                                    removeComments: true,
-                                    collapseWhitespace: true,
-                                    removeRedundantAttributes: true,
-                                    useShortDoctype: true,
-                                    removeEmptyAttributes: true,
-                                    removeStyleLinkTypeAttributes: true,
-                                    keepClosingSlash: true,
-                                    minifyJS: true,
-                                    minifyCSS: true,
-                                    minifyURLs: true,
-                                },
-                            }
-                            : undefined
+                        {
+                            minify: false
+                        }
                     )
-                ),
+                )),
                 // Inlines the webpack runtime script. This script is too small to warrant
                 // a network request.
                 // https://github.com/facebook/create-react-app/issues/5358
@@ -630,22 +631,22 @@ module.exports = function (webpackEnv) {
                 // Otherwise React will be compiled in the very slow development mode.
                 new webpack.DefinePlugin(env.stringified),
                 // This is necessary to emit hot updates (CSS and Fast Refresh):
-                isEnvDevelopment && new webpack.HotModuleReplacementPlugin(),
+                //isEnvDevelopment && new webpack.HotModuleReplacementPlugin(),
                 // Experimental hot reloading for React .
                 // https://github.com/facebook/react/tree/master/packages/react-refresh
-                isEnvDevelopment &&
-                shouldUseReactRefresh &&
-                new ReactRefreshWebpackPlugin({
-                    overlay: {
-                        entry: webpackDevClientEntry,
-                        // The expected exports are slightly different from what the overlay exports,
-                        // so an interop is included here to enable feedback on module-level errors.
-                        module: reactRefreshOverlayEntry,
-                        // Since we ship a custom dev client and overlay integration,
-                        // the bundled socket handling logic can be eliminated.
-                        sockIntegration: false,
-                    },
-                }),
+                // isEnvDevelopment &&
+                // shouldUseReactRefresh &&
+                // new ReactRefreshWebpackPlugin({
+                //     overlay: {
+                //         entry: webpackDevClientEntry,
+                //         // The expected exports are slightly different from what the overlay exports,
+                //         // so an interop is included here to enable feedback on module-level errors.
+                //         module: reactRefreshOverlayEntry,
+                //         // Since we ship a custom dev client and overlay integration,
+                //         // the bundled socket handling logic can be eliminated.
+                //         sockIntegration: false,
+                //     },
+                // }),
                 // Watcher doesn't work well if you mistype casing in a path so we use
                 // a plugin that prints an error when you attempt to do this.
                 // See https://github.com/facebook/create-react-app/issues/240
@@ -660,8 +661,8 @@ module.exports = function (webpackEnv) {
                 new MiniCssExtractPlugin({
                     // Options similar to the same options in webpackOptions.output
                     // both options are optional
-                    filename: 'static/css/[name].[contenthash:8].css',
-                    chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+                    filename: theme.themeDir + 'css/[name].[contenthash:8].css',
+                    chunkFilename: theme.themeDir + 'css/[name].[contenthash:8].chunk.css',
                 }),
                 // Generate an asset manifest file with the following content:
                 // - "files" key: Mapping of all asset filenames to their corresponding
@@ -677,9 +678,12 @@ module.exports = function (webpackEnv) {
                             manifest[file.name] = file.path;
                             return manifest;
                         }, seed);
-                        const entrypointFiles = entrypoints.main.filter(
-                            fileName => !fileName.endsWith('.map')
-                        );
+
+                        const entrypointFiles = Object.keys(entrypoints)
+                            .reduce((previous, current) => {
+                                return [...previous, ...entrypoints[current]];
+                            }, [])
+                            .filter(filename => !filename.endsWith('.map'));
 
                         return {
                             files: manifestFiles,
